1a8e9070a1a6bf36cf343d4b9ad42623229631b3
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-09-04 10:44:20 +0700

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
+import { Route, Router, Routes } from "react-router";
 import "./App.css";
-import { useProducts } from "./hooks/product";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import Layout from "./components/Layout";
+import ProtectedRoute from "./ProtectedRoute";
+import DashBoardPage from "./pages/admin/DashBoardPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
-  const { data, loading, error } = useProducts();
-  if (loading) return <p>Dang tai...</p>;
-  if (error) return <p>{error}</p>;
   return (
-    <div>
-      {data.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-          <img src={item.thumbnail} alt={item.title} />
-        </div>
-      ))}
-    </div>
+    <>
+      <Routes>
+        {/* Client */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="san-pham" element={<ProductPage />} />
+          <Route path="san-pham/:id" element={<ProductDetailPage />} />
+          <Route path="lien-he" element={<ContactPage />} />
+          <Route path="ve-chung-toi" element={<AboutPage />} />
+        </Route>
+
+        {/* Admin */}
+        <Route path="/admin" element={<ProtectedRoute />}>
+          <Route index element={<DashBoardPage />} />
+        </Route>
+
+        {/* Empty layout */}
+        <Route path="dang-ky" element={<RegisterPage />} />
+        <Route path="dang-nhap" element={<LoginPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
 
--- src/components/Child.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Child = ({ count }) => {
-  console.log(count);
-  return <div>Child</div>;
-};
-
-export default Child;
